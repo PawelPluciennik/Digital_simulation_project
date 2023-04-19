95524d39c3536481836e12bfb26f4654c8a240e1
LAPTOP-T0FJR96E\Paweł <pawciopl62@gmail.com>
2023-04-19 21:10:16 +0200

    Initial Commit

---
+++ Const.py
@@ -0,0 +1,8 @@
+class Const:
+    DISTANCE = 5000
+    START_DISTANCE = 2000
+    PERIOD = 0.02
+    DELTA = 8
+    N = 80
+    MEAN = 0
+    STD_DEV = 4
---
+++ User/User.py
@@ -0,0 +1,6 @@
+class User():
+  def __init__(self, position, power_A, power_B):
+    self.position = position
+    self.power_A = power_A
+    self.power_B = power_B
+
